bfc7847bc07b8f503550e1d2bfc81f4cb98680b4
mishagordeev <42177098+mishagordeev@users.noreply.github.com>
2022-02-20 12:25:00 +0300

    init

---
+++ init.js
@@ -0,0 +1,7 @@
+function init() {
+    drawBoard('board')
+    drawShape('shapes', 'cross', 'orange', 100, 100)
+    drawShape('shapes', 'circle', 'purple', 200, 200)
+    drawShape('shapes', 'rect', 'deepskyblue', 300, 300)
+    drawShape('shapes', 'triangle', 'tomato', 400, 400)
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+const a = 2 * Math.PI / 6;
+const r = 50;
+
+init();
+
